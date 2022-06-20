3e9488c4f34ef3b787debfca6648bb3a6f339ddc
Joel Lindberg <52636113+JoelLindberg@users.noreply.github.com>
2022-06-20 11:44:17 +0000

    Minor refactoring

--- timestamp/urls.py
+++ timestamp/urls.py
@@ -2,10 +2,14 @@ from django.urls import path, re_path
 
 from . import views
 
+re_unix_time = '^/(?P<unix_time>[0-9]{13}$)' # 1451001600000
+re_js_date1 = '^/(?P<iso_time>[0-9]{4}-[0-9]{2}-[0-9]{2}$)' # 2016-12-25
+re_js_date2 = '^/(?P<custom_time>[0-9]{2} [\w]+ [0-9]{4}, GMT$)' # "05 October 2011, GMT"
+
 urlpatterns = [
-  path('', views.current_date, name='current_date'),
-  path('/', views.current_date, name='current_date'),
-  path('/<int:unix_time>', views.requested_date, name='requested_date'),
-  path('/<slug:iso_time>', views.req_date_js, name='req_date_js'),
-  re_path('^/(?P<custom_time>[0-9]{2} [\w]+ [0-9]{4}, GMT)', views.req_date2_js, name='req_date2_js'),
+  re_path('^/$|^$', views.time_now, name='time_now'),
+  re_path(re_unix_time, views.unix_time, name='unix_time'),
+  re_path(re_js_date1, views.js_date1, name='js_date1'),
+  re_path(re_js_date2, views.js_date2, name='js_date2'),
+  path('/<slug:slug>', views.invalid_datetime, name='invalid_datetime'),
 ]
--- timestamp/views.py
+++ timestamp/views.py
@@ -2,14 +2,16 @@ from django.http import HttpResponse
 from datetime import datetime
 import json
 import time
-import re
 
 # Create your views here.
 
-def current_date(request):
+def time_now(request):
+  """Return the current time as unix time and utc in the format
+  :'Fri, 25 Dec 2015 00:00:00 GMT'."""
   unix_ms = time.time() * 1000
   utc = datetime.utcnow()
   utc = utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
+  
   d = {
     "unix": int(unix_ms),
     "utc": utc
@@ -19,9 +21,14 @@ def current_date(request):
                       content_type='application/json')
 
 
-def requested_date(request, unix_time):
+def unix_time(request, unix_time):
+  """Accept a unix (epoch) timestamp in millseconds.
+  :Return the same unix timestamp and an utc time in the format 
+  :'Fri, 25 Dec 2015 00:00:00 GMT'."""
+  unix_time = int(unix_time)
   epoch_s = unix_time / 1000
   utc = datetime.fromtimestamp(epoch_s)
+  
   d = {
       "unix": unix_time,
       "utc": utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
@@ -30,33 +37,41 @@ def requested_date(request, unix_time):
   return HttpResponse(json.dumps(d), content_type='application/json')
 
 
-def req_date_js(request, iso_time):
-  iso_p = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
-
-  r = re.search(iso_p, iso_time)
-  if r != None:
-    epoch_ms = time.mktime(time.strptime(iso_time, '%Y-%m-%d')) * 1000
-    utc = datetime.fromisoformat(iso_time)
-    d = {
-      "unix": int(epoch_ms),
-      "utc": utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    }
-  else:
-    d = { "error": "Invalid Date" }
+def js_date1(request, iso_time):
+  """Accept time in iso format and return unix time and utc time in the format
+  :'Fri, 25 Dec 2015 00:00:00 GMT'.
+  :The input time is one of many supported variants for Javascript's new Date()."""
+  epoch_ms = time.mktime(time.strptime(iso_time, '%Y-%m-%d')) * 1000
+  utc = datetime.fromisoformat(iso_time)
+  
+  d = {
+    "unix": int(epoch_ms),
+    "utc": utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
+  }
   
   return HttpResponse(json.dumps(d), 
                       content_type='application/json')
 
 
-def req_date2_js(request, custom_time):
+def js_date2(request, custom_time):
+  """Accept time in a Javascript specific time format and return unix time and utc time in the format
+  :'Fri, 25 Dec 2015 00:00:00 GMT'.
+  :The input time is one of many supported variants for Javascript's new Date()."""
   utc = datetime.strptime(custom_time, '%d %B %Y, %Z')
-
   epoch_ms = time.mktime(time.strptime(custom_time, '%d %B %Y, %Z')) * 1000
   
   d = {
     "unix": int(epoch_ms),
     "utc": utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
   }
+  
+  return HttpResponse(json.dumps(d), 
+                      content_type='application/json')
+
+
+def invalid_datetime(request, slug):
+  """Invalid inputs handled"""
+  d = { "error": "Invalid Date" }
   
   return HttpResponse(json.dumps(d), 
                       content_type='application/json')
